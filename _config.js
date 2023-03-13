beaf09147d19ab4615f73757d6ea31257f747d5f
Paulinemkavita <114083982+Paulinemkavita@users.noreply.github.com>
2023-03-13 21:31:07 +0300

    modified config.js

--- _config.js
+++ _config.js
@@ -4,6 +4,6 @@ var config = {}
 config.mongoURI = {
     production: 'mongodb+srv://paulinemkavita:Priscah@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
     development: 'mongodb+srv://paulinemkavita:Priscah@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv:paulinemkavita:Priscah@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    test: 'mongodb+srv://paulinemkavita:Priscah@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
 module.exports = config;
